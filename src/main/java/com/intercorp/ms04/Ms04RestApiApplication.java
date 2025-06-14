b72f28fb7b216516042cf9ecf27e8d8f7bd6b643
Leonardo <56612664+LeonardoSGSG@users.noreply.github.com>
2025-06-14 13:52:09 -0500

    1st commit

---
+++ src/main/java/com/intercorp/ms04/Ms04RestApiApplication.java
@@ -0,0 +1,13 @@
+package com.intercorp.ms04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ms04RestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ms04RestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/intercorp/ms04/Ms04RestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intercorp.ms04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ms04RestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
